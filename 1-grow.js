bc791975a7bec5f1116ff6d1ae21ce38f5a37a82
Joreinhard Munandar <jorensaragih@gmail.com>
2024-02-08 15:44:06 +1100

    Change 1

--- 1-grow.js
+++ 1-grow.js
@@ -1,9 +1,9 @@
 function grow(x){
     let total = x[0];
-    for(i=0;i<=x.length;i++){
-    total = total*x;
-    return total;
+    for(let i=1;i< x.length;i++){
+        total = total*x[i];
     }
+    return total;
 }
 
-console.log(grow([1,2,4,5]));
+console.log(grow([1,2,3,4]));
