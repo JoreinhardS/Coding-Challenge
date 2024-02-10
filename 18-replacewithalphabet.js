ce109170d0218464198159db3e3a63cd4ba9406c
Joreinhard Munandar <jorensaragih@gmail.com>
2024-02-11 01:34:28 +1100

    Challenge 18/366

--- 18-replacewithalphabet.js
+++ 18-replacewithalphabet.js
@@ -1,11 +1,13 @@
-function alphabetPosition(text) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    const newobj = {};
-    for (let i = 0; i < alphabet.length; i++) {
-        newobj[alphabet[i]] = i + 1; // Menggunakan i + 1 karena indeks dimulai dari 0
-    }
-    const mappedObj = text.toLowerCase().split('').map(char => newobj[char] || ' ').join(' ').replace(/ +/g, ' ').trim(); // Menggunakan ' ' jika karakter tidak ada di alphabet
-    return mappedObj; // Mengembalikan nilai mappedObj
-}
+// function alphabetPosition(text) {
+//     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+//     const newobj = {};
+//     for (let i = 0; i < alphabet.length; i++) {
+//         newobj[alphabet[i]] = i + 1; // Menggunakan i + 1 karena indeks dimulai dari 0
+//     }
+//     const mappedObj = text.toLowerCase().split('').map(char => newobj[char] || ' ').join(' ').replace(/ +/g, ' ').trim(); // Menggunakan ' ' jika karakter tidak ada di alphabet
+//     return mappedObj; // Mengembalikan nilai mappedObj
+// }
+
+const alphabetPosition = text => text.toLowerCase().split('').map(char => 'abcdefghijklmnopqrstuvwxyz'.indexOf(char) +1 || '').join(' ').replace(/ +/g, ' ').trim();
 
 console.log(alphabetPosition("The narwhal bacons at midnight."));
